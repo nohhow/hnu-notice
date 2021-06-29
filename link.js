e02166a398c2baf0c31dc757a0c0af9bcef5008a
nohhow <xksrma97@gmail.com>
2021-06-30 03:09:15 +0900

    모든 학과 게시판 추가 완료

--- link.js
+++ link.js
@@ -126,5 +126,115 @@ function getLink(click){
 function setMajor(){
 	if(getMajor == '국어국문∙창작학과'){
 		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=MTI3Mg==";
+	}else if(getMajor == '영어영문학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=MTAxOQ==";
+	}else if(getMajor == '일어일문학'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=ODA=";
+	}else if(getMajor == '프랑스어문학전공'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=OTE=";
+	}else if(getMajor == '문헌정보학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NjI=";
+	}else if(getMajor == '사학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=MTEwNQ==";
+	}else if(getMajor == '기독교학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NTU=";
+	}else if(getMajor == '국어교육과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NTE=";
+	}else if(getMajor == '영어교육과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?p_bdseq=77";
+	}else if(getMajor == '교육학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NTA=";
+	}else if(getMajor == '역사교육과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NzY=";
+	}else if(getMajor == '미술교육과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NjM=";
+	}else if(getMajor == '수학교육과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NzE=";
+	}else if(getMajor == '정보통신공학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=MTc2Mw==";
+	}else if(getMajor == '전기전자공학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?p_bdseq=81";
+	}else if(getMajor == '멀티미디어공학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NTk=";
+	}else if(getMajor == '건축학과(5년제)'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=MTEzNQ==";
+	}else if(getMajor == '건축공학전공'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=MTM4MQ==";
+	}else if(getMajor == '토목환경공학전공'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=ODk=";
+	}else if(getMajor == '기계공학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NTQ=";
+	}else if(getMajor == '화학공학과'){
+		document.querySelector('iframe').src = "http://chem-eng.hannam.ac.kr/%ea%b3%b5%ec%a7%80%ec%82%ac%ed%95%ad/";
+	}else if(getMajor == '신소재공학과'){
+		document.querySelector('iframe').src = "http://polymer.hannam.ac.kr/%ed%95%99%ea%b3%bc%ec%86%8c%ec%8b%9d/";
+	}else if(getMajor == '컴퓨터통신무인기술학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=MTIyNg==";
+	}else if(getMajor == '멀티미디어학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=MTIyOA==";
+	}else if(getMajor == '컴퓨터공학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NTY5";
+	}else if(getMajor == '산업경영공학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=Njc=";
+	}else if(getMajor == 'AI융합학과'){
+		document.querySelector('iframe').src = "http://smart.hannam.ac.kr/%ed%95%99%ea%b3%bc%ea%b3%b5%ec%a7%80/";
+	}else if(getMajor == '수학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?p_bdseq=731";
+	}else if(getMajor == '빅데이터응용학과'){
+		document.querySelector('iframe').src = "http://bigdata.hannam.ac.kr/%ea%b3%b5%ec%a7%80%ec%82%ac%ed%95%ad/";
+	}else if(getMajor == '경영학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NDc=";
+	}else if(getMajor == '회계학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=OTQ=";
+	}else if(getMajor == '무역학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=OTEx";
+	}else if(getMajor == '경제학전공'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=MjMx";
+	}else if(getMajor == '중국경제통상학전공'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=MTAxMQ==";
+	}else if(getMajor == '호텔항공경영학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=ODc=";
+	}else if(getMajor == '글로벌IT경영'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=MTM5MA==";
+	}else if(getMajor == '법학부'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NjQ=";
+	}else if(getMajor == '행정학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=OTI=";
+	}else if(getMajor == '경찰학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NDg=";
+	}else if(getMajor == '정치∙언론학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=ODQ=";
+	}else if(getMajor == '생명시스템과학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=Njg=" ;
+	}else if(getMajor == '식품영양학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?p_bdseq=72";
+	}else if(getMajor == '화학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NzMw";
+	}else if(getMajor == '간호학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NjI2";
+	}else if(getMajor == '스포츠과학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=Njk=";
+	}else if(getMajor == '바이오제약공학과'){
+		document.querySelector('iframe').src = "http://biopharm.hannam.ac.kr/%ea%b3%b5%ec%a7%80%ec%82%ac%ed%95%ad/";
+	}else if(getMajor == '사회복지학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NjY=";
+	}else if(getMajor == '아동복지학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NzQ=";
+	}else if(getMajor == '상담심리학과'){
+		document.querySelector('iframe').src = "http://counselpsy.hannam.ac.kr/%ea%b3%b5%ec%a7%80%ec%82%ac%ed%95%ad/";
+	}else if(getMajor == '사회적경제기업학과'){
+		document.querySelector('iframe').src = "http://seb.hannam.ac.kr/%ea%b3%b5%ec%a7%80%ec%82%ac%ed%95%ad/";
+	}else if(getMajor == '글로벌비즈니스'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=ODYz";
+	}else if(getMajor == '융합디자인학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NTg=";
+	}else if(getMajor == '회화과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=NzEy";
+	}else if(getMajor == '의류학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=MTQx";
+	}else if(getMajor == '미디어영상학과'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=MTU0NA==";
+	}else if(getMajor == '자유전공학부'){
+		document.querySelector('iframe').src = "http://uniboard.hannam.ac.kr/servlet/controller.helpdesk.UniboardServlet?seq=MTUzMA==";
 	}
 }
